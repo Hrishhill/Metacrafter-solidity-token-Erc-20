05de28717e252e52e2574e982d019426d21f7a33
Hrishhill <135793403+Hrishhill@users.noreply.github.com>
2024-09-18 18:43:02 +0530

    Update FanToken.sol

--- FanToken.sol
+++ FanToken.sol
@@ -19,4 +19,8 @@ contract FanToken is ERC20, Ownable {
     function burn(uint256 amount) public {
         _burn(msg.sender, amount);
     }
+function transfer(address recipient, uint256 amount) public override returns (bool) {
+    return super.transfer(recipient, amount);
 }
+
+
